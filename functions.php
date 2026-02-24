0843a98fb5e7f04becdb426f50602facd0a38f7d
Machiko Yamasaki <maruyama2150@gmail.com>
2026-02-24 16:10:49 +0900

    Add Theme setup to functions.php

--- functions.php
+++ functions.php
@@ -1,4 +1,26 @@
 <?php
+declare(strict_types=1);
+
+/**
+ * Theme setup
+ */
+add_action('after_setup_theme', function () {
+  add_theme_support('title-tag');
+  add_theme_support('post-thumbnails');
+  add_theme_support('html5', [
+    'search-form',
+    'comment-form',
+    'comment-list',
+    'gallery',
+    'caption',
+    'style',
+    'script',
+  ]);
+});
+
+/**
+ * Enqueue styles
+ */
 add_action('wp_enqueue_scripts', function () {
   wp_enqueue_style(
     'original-theme',
